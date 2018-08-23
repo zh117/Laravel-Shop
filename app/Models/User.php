01814f2d5cb64e8592fa84755df7f5868aa40b06
ZH117 <15319957499@163.com>
2018-08-23 08:46:01 +0000

    个人地址列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,4 +30,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function addresses()
+    {
+        return $this->hasMany(UserAddress::class);
+    }
+
 }
